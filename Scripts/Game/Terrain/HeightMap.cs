a742e3d5179c7c4db8497cc863625035dba02d39
mach <673363740@qq.com>
2019-03-12 00:01:01 +0800

    Map Generate

---
+++ Scripts/Game/Terrain/HeightMap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+namespace Assets.Scripts.Game.Terrain
+{
+    /// <summary>
+    /// 坐标高度图(x,z)实际代表(x+0.5,z+0.5)处的高度
+    /// </summary>
+    public class HeightMap
+    {
+        private Dictionary<Vector2Int, int> coordinateHeightMap = new Dictionary<Vector2Int, int>();
+
+        public int GetHeight(Vector2Int coordinate)
+        {
+            if (coordinateHeightMap.ContainsKey(coordinate)) return coordinateHeightMap[coordinate];
+            else
+            {
+                
+            }
+        }
+    }
+}
+
+
--- Scripts/Game/Terrain/MapGenerator.cs
+++ Scripts/Game/Terrain/MapGenerator.cs
@@ -7,24 +7,29 @@ using System.Threading.Tasks;
 
 namespace Assets.Scripts.Game.Terrain
 {
-    //chunk = 256*256*128 block
 
-
-    //choose map size
-    //create height-map, "Raising..."
-    //create strata, "Soiling..."
-    //carve out caves, "Carving..."
-    //create ore veins
-    //flood fill-water, "Watering..."
-    //flood fill-lava, "Melting..."
-    //create surface layer, "Growing..."
-    //create plants, "Planting..."
     public class MapGenerator : MonoSingleton<MapGenerator>
     {
+
+        private void CreateOneChunk()
+        {
+            
+        }
+
+        //chunk = 256*256*128 block
+        //choose map size
+        //create height-map, "Raising..."
+        //create strata, "Soiling..."
+        //carve out caves, "Carving..."
+        //create ore veins
+        //flood fill-water, "Watering..."
+        //flood fill-lava, "Melting..."
+        //create surface layer, "Growing..."
+        //create plants, "Planting..."
         private IEnumerator GenerateMap()
         {
             GenerateMapOperation generateMapOperation = new GenerateMapOperation();
-            
+
             yield return null;
         }
     }
