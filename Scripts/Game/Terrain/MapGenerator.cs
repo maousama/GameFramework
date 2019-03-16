027bda396c9d224430375951cc8648cff7a32ec0
maousama <673363740@qq.com>
2019-03-16 19:26:05 +0800

    biome

--- Scripts/Game/Terrain/MapGenerator.cs
+++ Scripts/Game/Terrain/MapGenerator.cs
@@ -24,19 +24,24 @@ namespace Assets.Scripts.Game.Terrain
                     chunk.Initialize(new Vector2Int(x, z));
                 }
             }
-            //float min = 0;
-            //float max = 0;
-            //for (int i = 0; i < 1000; i++)
-            //{
-            //    for (int j = 0; j < 1000; j++)
-            //    {
-            //        float value = PerlinNoise.SuperimposedOctave(seed2, i * 0.003f, j * 0.003f);
-            //        min = value < min ? value : min;
-            //        max = value > max ? value : max;
 
-            //    }
-            //}
-            //Debug.Log(min + " | | " + max);
+            if (false)
+            {
+                float min = 0;
+                float max = 0;
+                for (int i = 0; i < 1000; i++)
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        float value = PerlinNoise.SuperimposedOctave(seed2, i * 0.003f, j * 0.003f);
+                        min = value < min ? value : min;
+                        max = value > max ? value : max;
+
+                    }
+                }
+                Debug.Log(min + " | | " + max);
+            }
+
         }
 
         //chunk = 256*256*128 block
